7af6da598f90d1aee417379c9ccc46d75f4d66a1
LBockus <bockus.lukas@gmail.com>
2023-03-21 14:09:05 +0200

    added a choise between patient and doctor

---
+++ laravel_failai/app/Http/Controllers/Login/DoctorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
--- laravel_failai/resources/views/layouts/admin/header.blade.php
+++
@@ -1,9 +0,0 @@
-<header class="header header-light bg-primary header-sticky mb-4">
-    LOGO
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-
-        <button type="submit">Logout</button>
-    </form>
-    @include('layouts.admin.navigation')
-</header>
